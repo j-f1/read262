66f10e75a46d54eace370248d3b7b4e133ba31d6
Jed Fox <git@twopointzero.us>
2018-12-30 14:11:05 -0500

    Highlight <var>s on click

--- src/templates/section.tsx
+++ src/templates/section.tsx
@@ -3,6 +3,7 @@ import { graphql, navigate } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import SectionTitle from '../components/section-title'
 import { Edge, SitePage } from '../types'
 
 const SpecPage = ({
@@ -25,13 +26,21 @@ const SpecPage = ({
   React.useEffect(() => {
     const handler = (event: MouseEvent) => {
       const { target } = (event as unknown) as { target: HTMLElement }
-      if (
-        target &&
-        target.matches('article a') &&
-        target.getAttribute('href')
-      ) {
+      if (!target) return
+      if (target.matches('article a') && target.getAttribute('href')) {
         event.preventDefault()
         navigate(target.getAttribute('href')!)
+      } else if (target.matches('article var')) {
+        const name = target.textContent!
+        const shouldSelect = !target.classList.contains('referenced')
+        let clause = target
+        while (!clause.matches('emu-clause')) {
+          if (!clause.parentElement) return
+          clause = clause.parentElement
+        }
+        Array.from(clause.querySelectorAll('var'))
+          .filter(el => el.textContent === name)
+          .forEach(decl => decl.classList.toggle('referenced', shouldSelect))
       }
     }
     document.addEventListener('click', handler)
@@ -40,11 +49,12 @@ const SpecPage = ({
   return (
     <Layout>
       <SEO title={title} />
-      <h1>
-        <span className="secnum">{secnum}</span>
-        {title}
-      </h1>
-      <article dangerouslySetInnerHTML={{ __html: content }} />
+      <article>
+        <h1>
+          <SectionTitle title={title} secnum={secnum} />
+        </h1>
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+      </article>
     </Layout>
   )
 }
