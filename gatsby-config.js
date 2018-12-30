cd5dd50409864390fd641cbed91aa2879163d10b
Jed Fox <git@twopointzero.us>
2018-12-30 17:33:10 -0500

    Type check the TS when building the site

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-typescript`,
+    `gatsby-plugin-typescript-checker`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-manifest`,
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, StaticQuery, graphql } from 'gatsby'
+import { Link } from 'gatsby'
 
 import Header from './header'
 import './layout.css'
