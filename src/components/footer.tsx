89e24a05f51e62d4106829b73b870569bc10b633
Jed Fox <git@twopointzero.us>
2018-12-31 08:50:16 -0500

    Extract the footer to its own component

---
+++ src/components/footer.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Container from './container'
+
+const Footer = () => (
+  <footer>
+    <Container>
+      <a href="https://tc39.github.io/ecma262">Spec content</a> © 2018 Ecma
+      International (<Link to="/copyright-and-software-license">License</Link>)
+      • Site © 2018 <a href="https://github.com/j-f1">Jed Fox</a> (
+      <a href="https://github.com/j-f1/read262/blob/master/LICENSE">License</a>)
+      • Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
+      {process.env.NODE_ENV === 'development' && (
+        <>
+          {' '}
+          • <a href="/___graphql">GraphQL Explorer</a>
+        </>
+      )}
+    </Container>
+  </footer>
+)
+export default Footer
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,35 +1,15 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Header from './header'
 import './layout.css'
 import Container from './container'
+import Footer from './footer'
 
 const Layout = ({ children }: { children: React.ReactNode }) => (
   <>
     <Header />
-    <Container
-      style={{
-        paddingTop: 0,
-      }}
-    >
-      {children}
-      <footer>
-        <a href="https://tc39.github.io/ecma262">Spec content</a> © 2018 Ecma
-        International (<Link to="/copyright-and-software-license">License</Link>
-        ) • Site © 2018 <a href="https://github.com/j-f1">Jed Fox</a> (
-        <a href="https://github.com/j-f1/read262/blob/master/LICENSE">
-          License
-        </a>
-        ) • Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
-        {process.env.NODE_ENV === 'development' && (
-          <>
-            {' '}
-            • <a href="/___graphql">GraphQL Explorer</a>
-          </>
-        )}
-      </footer>
-    </Container>
+    <Container style={{ paddingTop: 0 }}>{children}</Container>
+    <Footer />
   </>
 )
 
