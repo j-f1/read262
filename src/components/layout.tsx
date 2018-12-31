6fa6da91fb9e48648c8883d805013b5f04f2e025
Jed Fox <git@twopointzero.us>
2018-12-31 09:30:55 -0500

    Add search via Lunr

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,11 +4,15 @@ import Header from './header'
 import './layout.css'
 import Container from './container'
 import Footer from './footer'
+import Search from './search'
 
 const Layout = ({ children }: { children: React.ReactNode }) => (
   <>
     <Header />
-    <Container style={{ paddingTop: 0 }}>{children}</Container>
+    <Container style={{ paddingTop: 0 }}>
+      <Search />
+      {children}
+    </Container>
     <Footer />
   </>
 )
