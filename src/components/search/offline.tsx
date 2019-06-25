2665ead0c7bf4dd88fa99221598c5a5aa2684e27
PrettierCI <commits@prettierci.com>
2019-06-25 15:02:19 +0000

    Sync with Prettier

--- src/components/search/offline.tsx
+++ src/components/search/offline.tsx
@@ -22,8 +22,9 @@ import { SearchProps } from '../search'
 
 const Highlight = ({ text, query }: { text: string; query: string }) => (
   <>
-    {parse(text, match(text, query)).map(({ text, highlight }, i) =>
-      highlight ? <mark key={i}>{text}</mark> : text
+    {parse(text, match(text, query)).map(
+      ({ text, highlight }, i) =>
+        highlight ? <mark key={i}>{text}</mark> : text
     )}
   </>
 )
@@ -68,19 +69,22 @@ const Search = ({ value, onChange }: SearchProps) => {
   )
 
   const [results, setResults] = useState(new Array<lunr.Index.Result>())
-  useEffect(() => {
-    if (value === '') {
-      setResults([])
-      return
-    }
-    let active = true
-    worker.search(value).then(results => {
-      if (active) setResults(results)
-    })
-    return () => {
-      active = false
-    }
-  }, [value])
+  useEffect(
+    () => {
+      if (value === '') {
+        setResults([])
+        return
+      }
+      let active = true
+      worker.search(value).then(results => {
+        if (active) setResults(results)
+      })
+      return () => {
+        active = false
+      }
+    },
+    [value]
+  )
 
   return (
     <div>
