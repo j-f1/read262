a6b9a993228d5513133fd6d0c4933894bd236e81
Jed Fox <git@jedfox.com>
2022-04-29 20:06:13 -0400

    Add sw.js to upgrade people still using the Gatsby PWA

---
+++ assets/root/sw.js
@@ -0,0 +1,14 @@
+// Remove the old Gatsby service worker
+
+self.addEventListener('install', () => {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', () => {
+  self.registration
+    .unregister()
+    .then(() => self.clients.matchAll())
+    .then((clients) => {
+      clients.forEach((client) => client.navigate(client.url))
+    })
+})
