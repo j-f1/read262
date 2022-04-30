7729545c41553554d1e6fd7429ece429701c45d9
Jed Fox <git@jedfox.com>
2022-04-29 22:10:37 -0400

    Update about page

--- pages/spec-section.jsx
+++ pages/spec-section.jsx
@@ -41,9 +41,9 @@ exports.default = async ({ section, sections }) => {
             <h2>About read262</h2>
             <p>
               read262 was originally created using Gatsby, but was ported to{' '}
-              <a href="https://11ty.dev">Eleventy</a> in early 2022. Some of
-              read262’s CSS originated in the Gatsby blog starter I began the
-              project with, while other CSS was copied over from the{' '}
+              <a href="https://11ty.dev">Eleventy</a> in 2022. Some of read262’s
+              CSS originated in the Gatsby blog starter I began the project
+              with, while other CSS was copied over from the{' '}
               <code>ecmarkup</code> repository (see{' '}
               <code>src/templates/ecmarkup.css</code>), and the rest is my own.
             </p>
@@ -70,16 +70,8 @@ exports.default = async ({ section, sections }) => {
                 Fira<span style={{ fontFamily: 'var(--sans)' }}> </span>Code
               </a>
               . Search is provided by{' '}
-              <a href="https://www.algolia.com">Algolia</a> when you’re online,
-              and{' '}
-              <a
-                href="https://github.com/bvaughn/js-search"
-                style={{ fontFamily: 'var(--mono)', fontWeight: 550 }}
-              >
-                js-search
-              </a>{' '}
-              when you’re offline. Everything is hosted on{' '}
-              <a href="https://www.netlify.com">Netlify</a>, and{' '}
+              <a href="https://www.algolia.com">Algolia</a>, and the site is
+              hosted on GitHub Pages.
               <a href="https://zapier.com">Zapier</a> is used to automate
               deploys whenever the spec repo is updated (
               <a href="https://zapier.com/shared/7baef3f269fd297f91fbeae398a4dca8d370d1b7">
@@ -87,10 +79,7 @@ exports.default = async ({ section, sections }) => {
               </a>
               ). The yellow color is copied from the JS logo.
             </p>
-            <p>
-              read262 supports Dark Mode automatically, and uses a service
-              worker to enable offline support.
-            </p>
+            <p>read262 supports Dark Mode automatically.</p>
           </>
         ) : null}
       </article>
